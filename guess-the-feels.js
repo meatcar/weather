3e681f93e8593576ad28b26fbf926be5383474be
Denys Pavlov <denys.pavlov@gmail.com>
2015-02-08 15:12:04 -0800

    moving towards dynamic locations

--- guess-the-feels.js
+++ guess-the-feels.js
@@ -1,36 +1,65 @@
 Forecasts = new Mongo.Collection("forecasts");
 
 if (Meteor.isClient) {
-  Meteor.subscribe("forecast");
+  Session.setDefault('degreeType', 'C');
+  Session.setDefault('location', '');
+
+  var getCurPosition = Session.get('location') === '';
+  window.navigator.geolocation.watchPosition(function (position) {
+    if (getCurPosition) {
+      Session.set('location', position.coords.latitude + ', ' + position.coords.longitude);
+      getCurPosition = !getCurPosition;
+    }
+  });
+
+  Meteor.subscribe("forecast", [Session.get('location'), Session.get('degreeType')]);
 
   Template.body.helpers({
     forecasts: function () {
       return Forecasts.find({}, {limit: 1});
     }
-  })
+  });
+
+  Template.forecast.helpers({
+    degreeType: function () {return Session.get('degreeType');}
+  });
+
+  Template.forecast.events({
+    "keydown .location": function (e) {
+      if (e.key != 'Enter') {
+        return;
+      }
+
+      Session.set('location', e.target.value);
+    },
+    "click .degree-type": function (e) {
+      var flip = {'C': 'F', 'F': 'C'};
+      Session.set('degreeType', flip[Session.get('degreeType')]);
+    }
+  });
 }
 
 if (Meteor.isServer) {
-  Meteor.publish("forecast", function () {
-    return Forecasts.find({}, {limit: 1})
-  })
-
-  Meteor.startup(function () {
-    if (Forecasts.find({}).count() != 0) {
+  Meteor.publish("forecast", function (location, degreeType) {
+    if (!location) {
       return;
     }
 
-    weatherjs.find({search: 'Toronto, ON, Canada', degreeType: 'C'},
-      Meteor.bindEnvironment(function (err, result) {
-        if (err) {
-          throw err;
-        }
+    console.log(arguments);
+    return Forecasts.find({}, {limit: 1});
+  });
 
-        var data = result[0].current;
-        data.formattedDate = moment(data.date, 'YYYY-MM-DD').format('dddd MMMM Do YYYY');
+  Meteor.startup(function () {
+    if (Forecasts.find().count() !== 0) {
+      return;
+    }
 
-        console.log(data);
-        Forecasts.insert(data);
-      }));
+    var data = Meteor.wrapAsync(weatherjs.find, weatherjs)
+                               ({search: 'Toronto, ON, Canada', degreeType: 'C'});
+
+    data = data[0];
+    data.current.formattedDate = moment(data.date, 'YYYY-MM-DD').format('dddd MMMM Do YYYY');
+
+    Forecasts.insert(data);
   });
 }
